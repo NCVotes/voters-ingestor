6ec78b90c4cee83b0765b1f14c032244f6ec79a9
Caleb Smith <caleb.smithnc@gmail.com>
2017-04-29 16:01:31 -0400

    Updates settings to use Postgres

--- ncvoter/settings.py
+++ ncvoter/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'ncvoter.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ncvoter',
+        'USER': 'ncvoter',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 

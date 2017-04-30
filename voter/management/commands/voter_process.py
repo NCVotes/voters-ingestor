4b72cf9c44cd17e0cfe02e4b127d9f90aae66f43
Caleb Smith <caleb.smithnc@gmail.com>
2017-04-29 22:54:55 -0400

    Adds existing instance query to voter_process and notes for procedure:
    Marks TODO's for various ChangeTracker and FileTracker updates needed

--- voter/management/commands/voter_process.py
+++ voter/management/commands/voter_process.py
@@ -24,20 +24,42 @@ def get_file_lines(filename):
             yield non_empty_row
 
 
+def find_model_and_existing_instance(file_tracker, row):
+    if file_tracker.data_file_kind == FileTracker.DATA_FILE_KIND_NCVOTER:
+        model_class = NCVoter
+        ncid = row['ncid']
+        instance = model_class.objects.filter(ncid=ncid).first()
+    elif file_tracker.data_file_kind == FileTracker.DATA_FILE_KIND_NCVHIS:
+        model_class = NCVHis
+        ncid = row['ncid']
+        election_desc = row['election_desc']
+        instance = model_class.objects.filter(ncid=ncid,
+                                                election_desc=election_desc).first()
+    else:
+        print("Unknown file format, aborting processing of {0}".format(file_tracker.filename))
+        return None, None
+    return model_class, instance
+
+
 def process_file(file_tracker):
-    filename = file_tracker.filename
-    for index, row in enumerate(get_file_lines(filename)):
+    for index, row in enumerate(get_file_lines(file_tracker.filename)):
         hash_val = find_md5(row)
-        if file_tracker.data_file_kind == FileTracker.DATA_FILE_KIND_NCVOTER:
-            model_class = NCVoter
-        elif file_tracker.data_file_kind == FileTracker.DATA_FILE_KIND_NCVHIS:
-            model_class = NCVHis
-        else:
-            print("Unknown file format, aborting processing of {0}".format(filename))
+        model_class, instance = find_model_and_existing_instance(file_tracker, row)
+        if model_class is None:
+            # Can't determine file type, cancel processing
             return
-        # TODO: Search for nc_id in existing data, compare against hash_val
+        if instance is not None and instance.md5_hash == hash_val:
+            # Nothing to do, data is up to date, move to next row
+            continue
         parsed_row = model_class.parse_row(row)
+        if instance is None:
+            pass
+            # TODO: Create ChangeTracker with op_code A here
+        else:
+            pass
+            # TODO: Create ChangeTracker with op_code M here
         print(hash_val, parsed_row)
+    # TODO: Mark file_tracker.change_tracker_processed=True
 
 
 class Command(BaseCommand):

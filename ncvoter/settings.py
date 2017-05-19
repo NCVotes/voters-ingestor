db982f9f70f0eb1f697480f58c165996bae3b8bb
Caleb Smith <caleb.smithnc@gmail.com>
2017-05-19 11:45:10 -0400

    WIP - Adds quickstart API with /users endpoint

--- ncvoter/settings.py
+++ ncvoter/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Third party apps
+    'rest_framework',
     # Internal
     'voter',
 ]
@@ -124,3 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}

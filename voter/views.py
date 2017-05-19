c86cfa96310a1d0bc246135aa6a76a4d9b63e32e
Caleb Smith <caleb.smithnc@gmail.com>
2017-05-19 13:16:51 -0400

    Adds custom FilterSet to allow for range search on created field

--- voter/views.py
+++ voter/views.py
@@ -1,12 +1,21 @@
 from rest_framework import viewsets
+import django_filters
 
 
 from voter.models import FileTracker
 from voter.serializers import FileTrackerSerializer
 
 
+class FileTrackerFilterSet(django_filters.FilterSet):
+    created = django_filters.DateTimeFromToRangeFilter()
+
+    class Meta:
+        model = FileTracker
+        fields = ('created', 'data_file_kind', 'change_tracker_processed',
+                  'updates_processed',)
+
+
 class FileTrackerViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = FileTracker.objects.all()
     serializer_class = FileTrackerSerializer
-    filter_fields =  ('created', 'data_file_kind', 'change_tracker_processed',
-                      'updates_processed',)
+    filter_class = FileTrackerFilterSet

416cc124f1a4c5b0611b2e99e48fe22a2a35d2f5
Caleb Smith <caleb.smithnc@gmail.com>
2017-05-28 16:27:35 -0400

    Initial fixes to gunicorn and nginx so gunicorn will run

--- ncvoter/wsgi.py
+++ ncvoter/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ncvoter.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ncvoter.prod_settings")
 
 application = get_wsgi_application()

988bcadb3f48db921cb53aacb709565b95e9c717
Calvin Spealman <calvin@caktusgroup.com>
2018-03-13 09:59:33 -0400

    Not uncommenting actual comments

--- run_tests.sh
+++ run_tests.sh
@@ -5,6 +5,6 @@ flake8 .
 
 coverage erase
 python manage.py makemigrations --dry-run --check
-TODO: Uncomment after getting tests running
+# TODO: Uncomment after getting tests running
 coverage run manage.py test --keepdb --noinput "$@"
 coverage report -m --skip-covered --fail-under 10

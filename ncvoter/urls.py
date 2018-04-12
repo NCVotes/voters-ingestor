68aa1e89fc6255ba2d6561a5dd1a7e05e20afa4a
Calvin Spealman <calvin@caktusgroup.com>
2018-04-12 09:22:23 -0400

    Do not need raw strings for new url path() helper

--- ncvoter/urls.py
+++ ncvoter/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 from django.contrib import admin
 
 urlpatterns = [
-    path(r'admin/', admin.site.urls),
-    path(r'api/v1/', include('voter.urls'))
+    path('admin/', admin.site.urls),
+    path('api/v1/', include('voter.urls'))
 ]

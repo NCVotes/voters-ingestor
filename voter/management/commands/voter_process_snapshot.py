480a54c1cbbd3ed4f7306da8c66b8aa2e811de2b
Vinod Kurup <vinod@kurup.com>
2018-05-03 11:36:30 -0400

    Use `out` helper in our other management command

--- voter/management/commands/voter_process_snapshot.py
+++ voter/management/commands/voter_process_snapshot.py
@@ -9,7 +9,7 @@ import traceback
 from bencode import bencode
 
 from voter.models import FileTracker, ChangeTracker, NCVoter, BadLineRange, BadLineTracker
-from voter.utils import tqdm_or_quiet
+from voter.utils import out, tqdm_or_quiet
 
 BULK_CREATE_AMOUNT = 500
 
@@ -145,8 +145,7 @@ def get_file_lines(filename, output):
         yield counted, row, non_empty_row
 
     bad_lines.flush()
-    if output:
-        print("Decoded", counted, "lines from", filename)
+    out("Decoded {} lines from {}".format(counted, filename), output)
 
 
 def find_existing_instance(ncid):
@@ -293,8 +292,7 @@ def track_changes(file_tracker, output):
 
     line_no = 0
 
-    if output:
-        print("Tracking changes for file {0}".format(file_tracker.filename), flush=True)
+    out("Tracking changes for file {0}".format(file_tracker.filename), output)
 
     # Have we seen any lines, successful or failures, from this before?
     prev_line = ChangeTracker.objects.filter(file_tracker=file_tracker).order_by('file_lineno').last()
@@ -348,15 +346,13 @@ def track_changes(file_tracker, output):
 
     # TODO: Add a way to skip this, if we want to re-run for testing without re-downloading
     # remove_files(file_tracker)
-    if output:
-        print("Lines processed for %s: %d" % (file_tracker.filename, line_no))
+    out("Lines processed for {}: {}".format(file_tracker.filename, line_no), output)
     return (added_tally, modified_tally, already_seen_tally, skip_tally)
 
 
 def process_files(**options):
     output = not options.get('quiet')
-    if output:
-        print("Processing NCVoter file...", flush=True)
+    out("Processing NCVoter file...", output)
 
     file_tracker_filter_data = {
         'data_file_kind': FileTracker.DATA_FILE_KIND_NCVOTER
@@ -370,8 +366,7 @@ def process_files(**options):
     for file_tracker in ncvoter_file_trackers:
         reset()
         if FileTracker.objects.filter(file_status=FileTracker.PROCESSING).exists() and not options.get('resume'):
-            if output:
-                print("Another parser is processing the files. Restart me later!")
+            out("Another parser is processing the files. Restart me later!", output)
             return
         lock_file(file_tracker)
         try:
@@ -391,19 +386,17 @@ def process_files(**options):
         # and vice-versa
         num_restored = NCVoter.objects.filter(ncid__in=processed_ncids).exclude(deleted=False).update(deleted=False)
 
-        if output:
-            print("Change tracking completed for {}:".format(file_tracker.filename))
-            print("Added records: {0}".format(added))
-            print("Modified records: {0}".format(modified))
-            print("Skipped records: {0}".format(skipped))
-            print("Already seen records: {0}".format(already_seen))
-            print("Restored records: {0}".format(num_restored))
-            print("Deleted voters: {0}".format(num_deleted), flush=True)
+        out("Change tracking completed for {}:".format(file_tracker.filename), output)
+        out("Added records: {0}".format(added), output)
+        out("Modified records: {0}".format(modified), output)
+        out("Skipped records: {0}".format(skipped), output)
+        out("Already seen records: {0}".format(already_seen), output)
+        out("Restored records: {0}".format(num_restored), output)
+        out("Deleted voters: {0}".format(num_deleted), output)
 
 
 def remove_files(file_tracker, output=True):
-    if output:
-        print("Deleting processed file {}".format(file_tracker.filename), flush=True)
+    out("Deleting processed file {}".format(file_tracker.filename), output)
     try:
         os.remove(file_tracker.filename)
     except FileNotFoundError:

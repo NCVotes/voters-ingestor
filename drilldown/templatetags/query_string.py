8709edfd1da9439b991c3dc3796e7f90dced1899
Calvin Spealman <calvin@caktusgroup.com>
2018-05-24 11:36:42 -0400

    [DIS-258] Some tests of course

--- drilldown/templatetags/query_string.py
+++ drilldown/templatetags/query_string.py
@@ -5,18 +5,6 @@ from django.utils.safestring import mark_safe
 register = template.Library()
 
 
-@register.simple_tag
-def query_string(request, **kwargs):
-    """usage: {% query_string request page=1 %}"""
-
-    updated = request.GET.copy()
-
-    for k, v in kwargs.iteritems():
-        updated[k] = v
-
-    return mark_safe(updated.urlencode())
-
-
 @register.filter
 def qs_trim(request, remove):
     """usage: {{ request|qs_trim:"key_to_remove" }}"""

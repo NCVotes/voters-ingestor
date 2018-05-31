0b5cfff8c0289b512ee27edc5340834f5ebdce36
Calvin Spealman <calvin@caktusgroup.com>
2018-05-31 08:39:09 -0400

    Add parition() tests

--- matview/tests.py
+++ matview/tests.py
@@ -2,7 +2,35 @@ from django.db import connection
 from django.test import TestCase
 
 from voter.models import NCVoter
+from .models import partition
 from . import dbutils
+
+
+class UtilTests(TestCase):
+
+    def test_partition_evenly(self):
+        numbers = list(range(1, 17))
+        groups = partition(numbers, 4)
+        self.assertEqual(groups[0], [1, 2, 3, 4])
+        self.assertEqual(groups[1], [5, 6, 7, 8])
+        self.assertEqual(groups[2], [9, 10, 11, 12])
+        self.assertEqual(groups[3], [13, 14, 15, 16])
+
+    def test_partition_short(self):
+        numbers = list(range(1, 16))
+        groups = partition(numbers, 4)
+        lengths = [len(group) for group in groups]
+        self.assertEqual(4, len(groups))
+        self.assertEqual(1, lengths.count(3))
+        self.assertEqual(3, lengths.count(4))
+    
+    def test_partition_long(self):
+        numbers = list(range(1, 18))
+        groups = partition(numbers, 4)
+        lengths = [len(group) for group in groups]
+        self.assertEqual(4, len(groups))
+        self.assertEqual(1, lengths.count(5))
+        self.assertEqual(3, lengths.count(4))
 
 
 class MatViewNameTests(TestCase):

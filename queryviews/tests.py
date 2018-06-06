aa5b64bcbcec7e1daae4865d1ba3135b1be9af9b
Calvin Spealman <calvin@caktusgroup.com>
2018-06-06 08:58:02 -0400

    [DIS-263] Add tests to ensure the correctly efficient paths are used depending on the relative sizes of flag and non-flag filters

--- queryviews/tests.py
+++ queryviews/tests.py
@@ -1,6 +1,7 @@
 from unittest.mock import patch
 
 from django.test import TestCase
+from django.apps import apps
 
 from voter.models import NCVoter
 from matview.models import MatView
@@ -117,29 +118,28 @@ class QueryTestCase(TestCase):
 
 
 class FlagTests(TestCase):
-    signal_setup = False
+    ncid = 1
 
     def add_voter_race_flags(self, voter):
         for raceflag in RaceFilter.get_raceflags(voter=voter):
             voter.data[raceflag] = "true"
 
-    def setUp(self):
-        ncid = 1
-        for rc, rl, rd in RaceFilter.RACES:
-            for party in ('DEM', 'REP'):
+    def create_some_voters(self, parties=('DEM', 'REP'), races=tuple(_[0] for _ in RaceFilter.RACES)):
+        for rc in races:
+            for party in parties:
                 data = {
                     "party_cd": party,
                     "race_code": rc,
                 }
-                voter = NCVoter.objects.create(ncid=str(ncid), data=data)
+                voter = NCVoter.objects.create(ncid=str(self.ncid), data=data)
                 self.add_voter_race_flags(voter)
                 voter.save()
-                ncid += 1
+                self.ncid += 1
 
         MatView.refresh_all()
 
-        if not self.signal_setup:
-            self.signal_setup = True
+    def setUp(self):
+        self.create_some_voters()
 
     def test_flags_exist(self):
         assert NCVoter.objects.filter(data__race_code='W').first().data['raceflag_w'] == 'true'
@@ -234,3 +234,62 @@ class FlagTests(TestCase):
             ncid_set.discard(sample[2].ncid)
             ncid_set.discard(sample[3].ncid)
             assert len(ncid_set) == 8
+
+    def test_count_other_facet_smaller(self):
+        """Make sure the flag logic doesn't break out query-smallest-first logic.
+
+        In this test we perform an aggregate count query by including three race codes,
+        but we set it up so the *other* field in the filter has a small materialized view.
+        We assert this other materialized view is used in this case and *not* our
+        flag views.
+        """
+
+        codes = [_[0] for _ in RaceFilter.RACES]
+        self.create_some_voters(parties=('REP',), races=codes * 5)
+        dem = apps.get_model("queryviews", "matview_mv_voter_ncvoter_party_cd_dem")
+        ab = apps.get_model("queryviews", "matview_mv_voter_ncvoter_raceflag_ab_true")
+        io = apps.get_model("queryviews", "matview_mv_voter_ncvoter_raceflag_io_true")
+        orig_filter = dem.objects.filter
+
+        with patch.object(dem.objects, 'filter') as dem_filter:
+            with patch.object(ab.objects, 'filter') as ab_filter:
+                with patch.object(io.objects, 'filter') as io_filter:
+                    dem_filter.side_effect = orig_filter
+                    ab_filter.side_effect = Exception
+                    io_filter.side_effect = Exception
+
+                    count = models.get_count("voter.NCVoter", {"party_cd": "DEM", "race_code": ['A', 'I', 'B']})
+
+                    assert count == 3
+                    assert dem_filter.call_count == 2
+
+
+    def test_count_other_facet_larger(self):
+        """Make sure the flag logic is used when they are more efficient.
+
+        In this test we perform an aggregate count query by including three race codes,
+        but we set it up so the *other* field in the filter has a larger materialized view.
+        We assert this other materialized view is *not* used in this case and both our
+        flag views are.
+        """
+
+        dem = apps.get_model("queryviews", "matview_mv_voter_ncvoter_party_cd_dem")
+        ab = apps.get_model("queryviews", "matview_mv_voter_ncvoter_raceflag_ab_true")
+        io = apps.get_model("queryviews", "matview_mv_voter_ncvoter_raceflag_io_true")
+
+        orig_ab_filter = ab.objects.filter
+        orig_io_filter = io.objects.filter
+
+        with patch.object(dem.objects, 'filter') as dem_filter:
+            with patch.object(ab.objects, 'filter') as ab_filter:
+                with patch.object(io.objects, 'filter') as io_filter:
+                    dem_filter.side_effect = Exception
+                    ab_filter.side_effect = orig_ab_filter
+                    io_filter.side_effect = orig_io_filter
+
+                    count = models.get_count("voter.NCVoter", {"party_cd": "DEM", "race_code": ['A', 'I', 'B', 'O']})
+
+                    assert count == 4
+                    assert dem_filter.call_count == 0
+                    assert ab_filter.call_count == 1
+                    assert io_filter.call_count == 1

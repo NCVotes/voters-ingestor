e7747a8dd90e7c3ec20aff2192d6a916c8d49812
philtuskha <phil.tuskha@gmail.com>
2018-06-28 10:06:15 -0400

    cleanup unique fields method

--- drilldown/utils.py
+++ drilldown/utils.py
@@ -7,10 +7,8 @@ def ordered_unique_keys(query_string):
         string passed in.  The values, in this case, are igonored.
     '''
     all_request_fields = [x[0] for x in parse_qsl(query_string)]
-    unique_fields = set(all_request_fields)
-    request_fields = []
+    unique_fields = []
     for field in all_request_fields:
-        if field in unique_fields:
-            request_fields.append(field)
-            unique_fields.remove(field)
-    return request_fields
+        if field not in unique_fields:
+            unique_fields.append(field)
+    return unique_fields
